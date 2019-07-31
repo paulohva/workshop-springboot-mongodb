86a0277ecfce5e9ee889d8904fb9a46a2819e165
Paulo Valadares <paulohva@gmail.com>
2019-07-31 23:52:30 +0100

    User resource + Rest

--- src/main/java/com/paulohva/workshopspringbootmongodb/WorkshopmongodbApplication.java
+++ src/main/java/com/paulohva/workshopspringbootmongodb/WorkshopmongodbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WorkshopmongodbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WorkshopmongodbApplication.class, args);
 	}
 
 }
