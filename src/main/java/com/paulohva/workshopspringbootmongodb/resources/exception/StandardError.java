fc26ac2b657adb1e92c7a1dafeb5bb417a3709db
Paulo Valadares <paulohva@gmail.com>
2019-08-01 16:19:37 +0100

    Busca por ID + Exception handler

---
+++ src/main/java/com/paulohva/workshopspringbootmongodb/resources/exception/StandardError.java
@@ -0,0 +1,68 @@
+package com.paulohva.workshopspringbootmongodb.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError() {
+
+    }
+
+    public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+	public Long getTimestamp() {
+		return this.timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return this.error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return this.path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+
+
+}
--- src/main/java/com/paulohva/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/paulohva/workshopspringbootmongodb/services/UserService.java
@@ -3,10 +3,13 @@ package com.paulohva.workshopspringbootmongodb.services;
 import com.paulohva.workshopspringbootmongodb.domain.User;
 import com.paulohva.workshopspringbootmongodb.repository.UserRepository;
 import com.paulohva.workshopspringbootmongodb.resources.UserResource;
+import com.paulohva.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,4 +20,10 @@ public class UserService {
     public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
