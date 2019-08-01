a4f3c7c1012a95f1566919fa7288f49d2ba8eeaa
Paulo Valadares <paulohva@gmail.com>
2019-08-01 21:06:43 +0100

    Endpoint para Post by ID

---
+++ src/main/java/com/paulohva/workshopspringbootmongodb/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.paulohva.workshopspringbootmongodb.resources;
+
+import com.paulohva.workshopspringbootmongodb.domain.Post;
+import com.paulohva.workshopspringbootmongodb.domain.User;
+import com.paulohva.workshopspringbootmongodb.dto.UserDTO;
+import com.paulohva.workshopspringbootmongodb.services.PostService;
+import com.paulohva.workshopspringbootmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value="/{id}",method=RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/paulohva/workshopspringbootmongodb/services/PostService.java
@@ -0,0 +1,30 @@
+package com.paulohva.workshopspringbootmongodb.services;
+
+import com.paulohva.workshopspringbootmongodb.domain.Post;
+import com.paulohva.workshopspringbootmongodb.domain.User;
+import com.paulohva.workshopspringbootmongodb.dto.UserDTO;
+import com.paulohva.workshopspringbootmongodb.repository.PostRepository;
+import com.paulohva.workshopspringbootmongodb.repository.UserRepository;
+import com.paulohva.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public List<Post> findAll() {
+        return repo.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
