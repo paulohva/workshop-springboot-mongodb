084209c5edac4dcb606fa20f692bfbcb3bce62a7
Paulo Valadares <paulohva@gmail.com>
2019-08-01 20:45:46 +0100

    Projeção dos dados do Author como DTO no POST

--- src/main/java/com/paulohva/workshopspringbootmongodb/Instantiation.java
+++ src/main/java/com/paulohva/workshopspringbootmongodb/Instantiation.java
@@ -2,6 +2,7 @@ package com.paulohva.workshopspringbootmongodb;
 
 import com.paulohva.workshopspringbootmongodb.domain.Post;
 import com.paulohva.workshopspringbootmongodb.domain.User;
+import com.paulohva.workshopspringbootmongodb.dto.AuthorDTO;
 import com.paulohva.workshopspringbootmongodb.repository.PostRepository;
 import com.paulohva.workshopspringbootmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,10 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
