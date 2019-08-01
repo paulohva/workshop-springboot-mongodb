3ff95742ce9f09f2adb4640969164f407f5db3e6
Paulo Valadares <paulohva@gmail.com>
2019-08-01 22:26:04 +0100

    Consulta simples com @Query

--- src/main/java/com/paulohva/workshopspringbootmongodb/repository/PostRepository.java
+++ src/main/java/com/paulohva/workshopspringbootmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.paulohva.workshopspringbootmongodb.repository;
 import com.paulohva.workshopspringbootmongodb.domain.Post;
 import com.paulohva.workshopspringbootmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
