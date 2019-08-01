3e6ed510f08cb3ca2a5d61582dd1e24e2027a504
Paulo Valadares <paulohva@gmail.com>
2019-08-01 22:06:42 +0100

    Post search by title using Query Methods

--- src/main/java/com/paulohva/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/paulohva/workshopspringbootmongodb/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.paulohva.workshopspringbootmongodb.resources;
 import com.paulohva.workshopspringbootmongodb.domain.Post;
 import com.paulohva.workshopspringbootmongodb.domain.User;
 import com.paulohva.workshopspringbootmongodb.dto.UserDTO;
+import com.paulohva.workshopspringbootmongodb.resources.util.URL;
 import com.paulohva.workshopspringbootmongodb.services.PostService;
 import com.paulohva.workshopspringbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,13 @@ public class PostResource {
     @RequestMapping(value="/{id}",method=RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch",method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> obj = service.findByTitle(text);
         return ResponseEntity.ok().body(obj);
     }
 }
